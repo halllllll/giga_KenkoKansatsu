016e80446c77ab776cfebeaa930d7dc566ef3d26
h4lllllll <hjod1172@yahoo.co.jp>
2023-02-27 04:28:25 +0000

    fix font

--- src/client/components/theme.tsx
+++ src/client/components/theme.tsx
@@ -1,16 +1,13 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
-  fonts: {
-    html: '"BIZ UDPGothic",Meiryo, sans-serif',
+  styles: {
+    global: {
+      "html, body": {
+        fontFamily: '"BIZ UDPGothic",Meiryo, sans-serif',
+      },
+    },
   },
-  // styles: {
-  //   global: {
-  //     "html, body": {
-  //       fontFamily: '"BIZ UDPGothic",Meiryo, sans-serif',
-  //     },
-  //   },
-  // },
 });
 
 export { theme };
