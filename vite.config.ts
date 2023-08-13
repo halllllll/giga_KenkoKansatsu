ee3574c25181e6463e752624dd28f38bcffcbd0d
halllllll <hjod1172@yahoo.co.jp>
2023-08-13 11:55:16 +0000

    will be killed

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import { resolve } from "path";
+
+const root = resolve(__dirname, "src");
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), viteSingleFile()],
+  root,
   build: {
     minify: true,
     outDir: "dist",
+    rollupOptions: {
+      input: {
+        app: resolve(root, "client", "index.html"),
+      },
+      external: [resolve(root, "client", "API", "stubs")],
+    },
   },
 });
