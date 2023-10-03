e0ec42c7fa01095a73f8d384271158612642931a
h4lllllll <hjod1172@yahoo.co.jp>
2023-10-03 09:43:36 +0000

    modify

--- src/customMenu/client/components/menuParts/ParentPassForm/ParentPassword.tsx
+++ src/customMenu/client/components/menuParts/ParentPassForm/ParentPassword.tsx
@@ -1,11 +1,15 @@
 import { type FC } from "react";
-import { Heading } from "@chakra-ui/react";
+import { Box, Center, Heading } from "@chakra-ui/react";
 
 const ParentPassword: FC = () => {
   return (
-    <>
-      <Heading>保護者用パスワード設定画面</Heading>
-    </>
+    <Box mx="auto">
+      <Box>
+        <Center pb={5}>
+          <Heading>保護者用パスワード設定画面</Heading>
+        </Center>
+      </Box>
+    </Box>
   );
 };
 
