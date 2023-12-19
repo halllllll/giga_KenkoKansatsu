b36e21993bbeb431323bb0866fda635a567dd009
h4lllllll <hjod1172@yahoo.co.jp>
2023-12-19 06:52:15 +0000

    modify

--- build.cjs
+++ build.cjs
@@ -1,17 +1,12 @@
 // build.cjs
 
 const { GasPlugin } = require('esbuild-gas-plugin');
-
 require('esbuild')
   .build({
     entryPoints: ['src/Main.ts'],
-
     bundle: true,
-
     outfile: 'dist/main.js',
-
     logLevel: 'info',
-
     plugins: [GasPlugin],
   })
   .catch(() => process.exit(1));
